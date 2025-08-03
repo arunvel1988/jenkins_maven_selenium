8214bfe8b75a41c43d072c10c2becbaef68a93bf
Arunvel Arunachalam <aryan.arunachalam@gmail.com>
2025-08-03 13:21:58 +0530

    Update Demo.java

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,8 +1,10 @@
+// File: Demo.java
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -12,35 +14,39 @@ public class Demo {
 
     public WebDriver driver;
 
-
     @BeforeClass
     public void beforeClass() {
-        // Setup ChromeDriver using WebDriverManager
+        // Setup FirefoxDriver using WebDriverManager
         WebDriverManager.firefoxdriver().setup();
 
-        // Create a new instance of the ChromeDriver
-        driver = new FirefoxDriver();
+        // Set Firefox to headless mode
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true); // or: options.addArguments("--headless");
 
-        // Navigate to a website
+        // Create a new instance of the FirefoxDriver with options
+        driver = new FirefoxDriver(options);
+
+        // Navigate to the website and perform login
         driver.get("https://www.saucedemo.com");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
     }
-        @Test
+
+    @Test
     public void test1() {
-        //System.out.println("test 1");
-
-            WebElement text = driver.findElement(By.xpath("//span[text()='Products']"));
-
-            String originalText = "Products";
-            String expectedText = text.getText();
-            Assert.assertEquals(originalText,expectedText);
+        // Verify that the Products page loaded
+        WebElement text = driver.findElement(By.xpath("//span[text()='Products']"));
+        String expectedText = "Products";
+        String actualText = text.getText();
+        Assert.assertEquals(actualText, expectedText);
     }
 
     @AfterClass
     public void afterClass() {
-        // Close the WebDriver instance
-        driver.quit();
+        // Close the browser and WebDriver
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
